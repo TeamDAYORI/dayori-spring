c538bd21b30b65a99c315818d2139aa8f9ed4dae
kosaComputerKww <dyffh1031@gmail.com>
2023-06-02 21:13:46 +0900

    create: [SpringBoot] 스프링 부트 프로젝트 생성 - kww

---
+++ spring-boot/src/main/java/com/tody/dayori/DayoriApplication.java
@@ -0,0 +1,13 @@
+package com.tody.dayori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayoriApplication.class, args);
+	}
+
+}
---
+++ spring-boot/src/test/java/com/tody/dayori/DayoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tody.dayori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
