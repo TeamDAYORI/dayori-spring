41e60d0b98fe6bc7bc2652ecfe9be7d28f26a647
unhhyyeexx <unhhyyee66@gmail.com>
2023-07-14 15:50:38 +0900

    feat: [Diary] diary엔티티에 초대코드(invitation code) 필드 생성  - jah

--- spring-boot/src/main/java/com/tody/dayori/diary/domain/Diary.java
+++ spring-boot/src/main/java/com/tody/dayori/diary/domain/Diary.java
@@ -1,11 +1,14 @@
 package com.tody.dayori.diary.domain;
 
 import lombok.*;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -37,14 +40,25 @@ public class Diary extends BaseEntity{
 
     private String diaryPassword;
 
+    @Column(nullable = false)
+    private String invitationCode;
+
     @OneToMany(mappedBy = "diary")
     private List<UserDiary> userDiaryList = new ArrayList<>();
+
+    public static String createInvitationCode(int length) {
+        byte[] uuidBytes = UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8);
+        byte[] base64Bytes = Base64.encodeBase64(uuidBytes);
+        String base64String = new String(base64Bytes, StandardCharsets.UTF_8).trim();
+        return base64String.substring(0, length);
+    }
 
     public static Diary create(String title, String cover, Integer duration, String password) {
         Diary diary = new Diary();
         diary.diaryTitle = title;
         diary.diaryCover = cover;
         diary.diaryDuration = duration;
+        diary.invitationCode = createInvitationCode(32);
         diary.diaryPassword = password;
         diary.diaryWithdraw = false;
         return diary;
