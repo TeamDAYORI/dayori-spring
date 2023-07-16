8f883637c807740394331c92d0d60e741915cb9e
jhkang9820 <jhkang9820@gmail.com>
2023-07-16 15:49:45 +0900

    feat: [Page] 페이지 수정 권한 조건문 추가 - kjh

--- spring-boot/src/main/java/com/tody/dayori/page/constant/PageConstant.java
+++ spring-boot/src/main/java/com/tody/dayori/page/constant/PageConstant.java
@@ -6,9 +6,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PageConstant {
     public static final String CREATE_PAGE_SUCCESS_MESSAGE = "페이지 등록에 성공했습니다.";
-    public static final String CREATE_PAGE_FAIL_MESSAGE = "페이지 등록에 실패했습니다.";
     public static final String SEARCH_PAGE_SUCCESS_MESSAGE = "페이지 조회에 성공했습니다.";
-    public static final String NOT_EXIST_PAGE_MESSAGE = "페이지가 존재하지 않습니다.";
     public static final String UPDATE_PAGE_SUCCESS_MESSAGE = "페이지 수정을 완료했습니다.";
     public static final String DELETE_PAGE_SUCCESS_MESSAGE = "페이지 삭제를 완료했습니다.";
+
+    public static final String CREATE_PAGE_FAIL_MESSAGE = "페이지 등록에 실패했습니다.";
+    public static final String UNAUTHORIZED_USER_ERROR_MESSAGE = "페이지 수정 및 삭제 권한이 없습니다.";
+    public static final String NOT_EXIST_PAGE_MESSAGE = "페이지가 존재하지 않습니다.";
 }
