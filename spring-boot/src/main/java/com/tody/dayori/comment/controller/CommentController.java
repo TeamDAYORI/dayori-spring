a9681586a710f9c055bdab9622d069f697dfa53c
jhkang9820 <jhkang9820@gmail.com>
2023-08-12 00:36:09 +0900

    feat: [Page] 현재 로그인한 유저 정보 확인 로직 추가 - kjh

--- spring-boot/src/main/java/com/tody/dayori/comment/controller/CommentController.java
+++ spring-boot/src/main/java/com/tody/dayori/comment/controller/CommentController.java
@@ -1,33 +1,39 @@
 package com.tody.dayori.comment.controller;
 
+import com.tody.dayori.auth.repository.UserRepository;
+import com.tody.dayori.auth.service.UserServiceImpl;
 import com.tody.dayori.comment.dto.CreateCommentRequest;
 import com.tody.dayori.comment.dto.DeleteCommentRequest;
 import com.tody.dayori.comment.dto.UpdateCommentRequest;
 import com.tody.dayori.comment.service.CommentService;
 import com.tody.dayori.common.dto.BaseResponse;
+import com.tody.dayori.common.exception.NotFoundException;
 import com.tody.dayori.page.domain.Page;
-import com.tody.dayori.page.service.PageService;
+import com.tody.dayori.page.service.PageServiceImpl;
 import com.tody.dayori.auth.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import static com.tody.dayori.comment.constant.CommentConstant.*;
+import static com.tody.dayori.common.util.TokenUtil.*;
 
 @Controller
 @RequestMapping("/comment")
 @RequiredArgsConstructor
 public class CommentController {
 
-    private final PageService pageService;
+    private final PageServiceImpl pageService;
     private final CommentService commentService;
+    private final UserRepository userRepository;
 
     @PostMapping
     public ResponseEntity<BaseResponse> createComment (@RequestBody CreateCommentRequest createCommentRequest) {
-        User user = new User();
-        user.setUserSeq(2L);
+        User user = userRepository.findById(getCurrentUserSeq())
+                .orElseThrow(NotFoundException::new);
         Page page = pageService.findPageById(createCommentRequest.getPageId());
 
         return new ResponseEntity<>(BaseResponse.from(
@@ -39,8 +45,9 @@ public class CommentController {
 
     @PutMapping
     public ResponseEntity<BaseResponse> updateComment (@RequestBody UpdateCommentRequest updateCommentRequest) {
-        // 수정 권한 확인
-        commentService.updateComment(updateCommentRequest);
+        User user = userRepository.findById(getCurrentUserSeq())
+                .orElseThrow(NotFoundException::new);
+        commentService.updateComment(updateCommentRequest, user);
         return new ResponseEntity<>(BaseResponse.from(
                 true,
                 UPDATE_COMMENT_SUCCESS_MESSAGE),
@@ -49,9 +56,8 @@ public class CommentController {
 
     @DeleteMapping
     public ResponseEntity<BaseResponse> deleteComment (@RequestBody DeleteCommentRequest deleteCommentRequest) {
-        User user = new User();
-        user.setUserSeq(2L);
-        // 삭제 권한 확인
+        User user = userRepository.findById(getCurrentUserSeq())
+                .orElseThrow(NotFoundException::new);
         commentService.deleteComment(deleteCommentRequest, user);
         return new ResponseEntity<>(BaseResponse.from(
                 true,
