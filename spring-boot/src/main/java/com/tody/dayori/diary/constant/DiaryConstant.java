0470ede2dcc6393f94df457f7231ce55c592a8ab
unhhyyeexx <unhhyyee66@gmail.com>
2023-08-11 19:57:32 +0900

    feat: [Diary] 유저 및 권한 관련 수정 및 response 메시지 수정

---
+++ spring-boot/src/main/java/com/tody/dayori/diary/constant/DiaryConstant.java
@@ -0,0 +1,15 @@
+package com.tody.dayori.diary.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DiaryConstant {
+    public static final String CREATE_DIARY_SUCCESS_MESSAGE = "다이어리 등록에 성공했습니다.";
+    public static final String JOIN_DIARY_SUCCESS_MESSAGE = "다이어리 가입에 성공했습니다.";
+    public static final String SEARCH_DIARY_SUCCESS_MESSAGE = "다이어리 조회에 성공했습니다.";
+    public static final String UPDATE_DIARY_SUCCESS_MESSAGE = "다이어리 수정을 완료했습니다.";
+    public static final String CREATE_DIARY_FAIL_MESSAGE = "다이어리 조회에 실패했습니다.";
+
+
+}
